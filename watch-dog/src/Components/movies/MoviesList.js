83062db48a6e2206d971f2a0107bd88b1823ff86
Ashon Woodbury <a.woodbury01@gmail.com>
2020-06-23 16:05:22 -0400

    finished headers on all pages. working on external api fetch

--- watch-dog/src/Components/movies/MoviesList.js
+++ watch-dog/src/Components/movies/MoviesList.js
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
-import { Header, Icon } from "semantic-ui-react";
+import { Header, Icon, Divider, Card } from "semantic-ui-react";
+import ContentManager from "../../modules/ContentManager"
+import MovieCard from "./MovieCard"
+import "../profile/Profile.css"
 
 class MoviesList extends Component {
   state = {
     movies: [],
   };
 
+  componentDidMount() {
+    ContentManager.getAll().then((movies) => {
+      
+      console.log(movies)
+      
+      this.setState({
+        movies: movies,
+      });
+    });
+  }
+
+
   render() {
     return (
       <>
-        <Header as="h2" icon>
-          <Icon name="film" />
-          Movies
-          <Header.Subheader>
-            List of Movies
-          </Header.Subheader>
-        </Header>
+        <div className="header-container">
+          <Header as="h2" icon>
+            <Icon name="film" />
+            Movies
+          </Header>
+        </div>
+
+        <Divider />
+
+        {/* <Card.Group className="watchlist-container">
+            {this.state.movies.results.locations.map(content => {
+              return (
+                <MovieCard
+                  key={content.id}
+                  movies={content}
+                  {...this.props}
+                />
+              )
+            })}
+        </Card.Group> */}
       </>
     )
   }
--- watch-dog/src/modules/ContentManager.js
+++ watch-dog/src/modules/ContentManager.js
@@ -1,3 +1,5 @@
+import API_KEY from "../../src/.env"
+
 const externalURL = "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com"
 
 const remoteURL = "http://localhost:5002"
@@ -5,5 +7,15 @@ const remoteURL = "http://localhost:5002"
 export default {
     getAllWatchList() {
         return fetch(`${remoteURL}/watchList`).then(result => result.json())
+    },
+    getAll() {
+        return fetch(`${externalURL}/lookup?term=bojack`, {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+                "x-rapidapi-key": `32fe133781mshf106f6b4306814cp181799jsn39f4c2b75902`
+            }
+        })
+        .then(result => result.json())
     }
 }
